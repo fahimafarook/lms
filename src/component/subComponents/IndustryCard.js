9c0355f216f62b52aab9bd9a0bc5c91d7f4a43c1
fahima farook <fahimafarook510@gmail.com>
2024-01-20 15:42:24 +0530

    --

--- src/component/subComponents/IndustryCard.js
+++ src/component/subComponents/IndustryCard.js
@@ -20,8 +20,8 @@ function IndustryCard(props) {
             scrub: true,
           },
          
-          // background: "linear-gradient(145deg, rgba(101, 87, 255, 1), rgba(170, 63, 255, 1) 48%, rgba(252, 247, 74, 0.7) 86%)",
-          background: "radial-gradient(circle, hsl(268, 90%, 49%) 50%, 0%, rgb(108, 0, 216) 40%)",
+          background: "linear-gradient(145deg, rgba(101, 87, 255, 1), rgba(170, 63, 255, 1) 48%, rgba(252, 247, 74, 0.7) 86%)",
+          // background: "radial-gradient(circle, hsl(268, 90%, 49%) 50%, 0%, rgb(108, 0, 216) 40%)",
           opacity:1,
         });
     
