2b79ddbd1330b7e1c721c3512c7ef88c0a7bc747
fahima farook <fahimafarook510@gmail.com>
2024-02-27 14:23:44 +0530

    changes

--- src/component/IndustrialPage.js
+++ src/component/IndustrialPage.js
@@ -117,14 +117,19 @@ function IndustrialPage() {
                   <GradientButton arrow ={true} name ="Book Demo" className = "industrial-book-demo"/>
                 </div>
             </div>
-            <div className='why-section'>
-                <h1 ref ={sectionRef} className='why-heading'>Why Real Estate CRM ?</h1>
-                <div ref ={sectionRef2} className='why-explanation'>A real estate CRM is a specialized tool designed for real estate agents, brokers, and agencies to help streamline sales processes, organize client information, and manage leads, properties, and appointments efficiently.</div>
-            </div>
+            <div className='why-section-parent'>
+              <div className='why-section'>
+                  <h1 ref ={sectionRef} className='why-heading'>Why Real Estate CRM ?</h1>
+                  <div ref ={sectionRef2} className='why-explanation'>A real estate CRM is a specialized tool designed for real estate agents, brokers, and agencies to help streamline sales processes, organize client information, and manage leads, properties, and appointments efficiently.</div>
+              </div>
+            
+            
 
             <div className='why-section'>
                 <h1 ref ={sectionRef3} className='why-heading'>Why is a Real Estate CRM important for Real Estate Agents?</h1>
                 <div ref ={sectionRef4} className='why-explanation'>Keeping track of team activity and performance, customer interactions, and managing sales pipeline is a huge challenge for all real estate agencies. And that's where a real estate CRM like TeleCRM can help. With a real estate CRM, you can</div>
+            </div>
+
             </div>
 
             <div className='with-crm'>
@@ -132,13 +137,16 @@ function IndustrialPage() {
                 <div className='tiny-cards-container row justify-content-center'> 
                     {
                          IndustryData.realEstate.miniCard.map((item, index) =>(
-                            <div className='tiny-card col-3 m-3'>
-                                <div className='tiny-card-title'>
+                            <div className='tiny-card col-3 m-3' style ={{padding:"0.5px"}}>
+                              <div className='card-foreground col-12'>
+                              <div className='tiny-card-title'>
                                     <img className='tiny-card-icon' src = {notepadIcon}></img>
                                     <div className='tiny-card-name'>{item.heading}</div>
                                 </div>
                                
                                 <div className='tiny-card-description'>{item.content}</div>
+                              </div>
+
                             </div>
                          ))
                     }
